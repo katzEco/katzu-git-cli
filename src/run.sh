f22b0909caf0b338343677b5dafe378a55390999
Suphakit P <me@suphakit.net>
2023-06-30 00:40:01 +0700

    feat<script>: setting up based script

---
+++ src/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+HOME_SOURCE='./scripts'
+
+source ${HOME_SOURCE}/function.sh
+
+if [[ -z $1 ]]; then
+  echo 'There is no arguments inputing here..'
+  source ${HOME_SOURCE}/help.sh
+else
+  if [[ $1 =~ ^[add]+$ || $1 =~ ^[a]+$ ]]; then
+    if [[ $2 =~ ^[.]+$ ]]; then
+      echo 'adding all files..'
+      source "${HOME_SOURCE}/add.sh" .
+    else
+      source $HOME_SOURCE/add.sh $(listArg "2" "$@")
+    fi
+  elif [[ $1 =~ ^[commit]+$ || $1 =~ ^[c]+$ ]]; then
+    if [[ -z $2 && -z $3 && -z $4 ]]; then
+      echo "Can't commiting blank commit.."
+      echo ""
+    else
+      source $HOME_SOURCE/commit.sh $(listArg "2" "$@")
+    fi
+  elif [[ $1 =~ ^[help]+$ || $1 =~ ^[h]+$ ]]; then
+    source ${HOME_SOURCE}/help.sh $1
+  else
+    echo "There is no argument '${1}'.."
+    source "${HOME_SOURCE}/help.sh"
+  fi
+fi
