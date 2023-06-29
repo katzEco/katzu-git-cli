8533eb8ad3e0d1fb4e4b6819a46a463c62c9df20
Suphakit P <me@suphakit.net>
2023-06-29 20:03:43 +0700

    chore: update commit handling

--- src/scripts/commit.sh
+++ src/scripts/commit.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ -z $1 ]]; then
-
+if [[ -z $1 && -z $2 && -z $3 ]]; then
+  if [[ $2 = ^[no]+$ || $2 = ^[idk]+$ ]]; then
+    git commit -m "$1: $3"
+  else
+    git commit -m "$1<$2>: $3"
+  fi
 fi
