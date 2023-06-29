a00e44c0559c3934f541b190ded8ed68fa5ac537
Suphakit P <me@suphakit.net>
2023-06-29 20:31:33 +0700

    feat: help script based

---
+++ src/scripts/help.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ -z $1 ]]; then
+  echo ""
+fi 
+
+echo "=============================================="
+echo "               katzu's Lazy git"
+echo ""
+echo "  [help / h]: a command to show this page"
+echo "  [add /a]: add file to your commit"
+echo "    example: kg a [filenames or '.']"
+echo "  [commit / c]: commit new file to your git repo"
+echo "    example: kg c [commit type] [scope (if don't have scope type 'no' or 'idk')] [subject]"
+echo ""
+echo "=============================================="
